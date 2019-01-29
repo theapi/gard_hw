b3cb7709b546157804727b20d11b262eaee3e412
theapi <petermclarke@gmail.com>
2019-01-29 22:40:29 +0000

    Water temperature

--- stm32/pump_controller/Inc/water.h
+++ stm32/pump_controller/Inc/water.h
@@ -10,6 +10,16 @@
 
 #include "stdint.h"
 
+// Thermistor datasheet http://uk.farnell.com/vishay-bc-components/ntcle100e3103jb0/thermistor-10k-5-ntc-rad/dp/1187031
+// resistance at 25 degrees C
+#define THERMISTOR_NOMINAL 10000.0
+// temp. for nominal resistance (almost always 25 C)
+#define THERMISTER_TEMPERATURE_NOMINAL 25.0
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define THERMISTOR_BCOEFFICIENT 3977.0
+// the value of the 'other' resistor
+#define THERMISTOR_SERIES_RESISTOR 10000.0
+
  /**
   * Temperature of the water.
   */
--- stm32/pump_controller/Src/water.c
+++ stm32/pump_controller/Src/water.c
@@ -1,12 +1,61 @@
 
-
+#include "math.h"
 #include "ads1015.h"
 #include "water.h"
 
 extern I2C_HandleTypeDef hi2c1;
 
 uint16_t WATER_Temperature() {
-    uint16_t val = ADS1015_SingleEnded(&hi2c1, ADS1015_ADDRESS, 1, ADS1015_GAIN_ONE);
-    //float mV = (float)val * 14.084507042;
-    return val;
+	/*
+	 * 1x gain   +/- 4.096V  1 bit = 2mv
+	 * 2x gain   +/- 2.048V  1 bit = 1mV
+	 *
+	 * 10k @ 25C
+	 * with 10k divider at 25C the adc should read 3300/2 = 1650mV
+	 * so 25C = 1650mV = 1650 bits
+	 * so 1C = 1650 / 25 = 66
+	 *
+	 * 1x gain:
+	 * so 25C = 1650mV = 825 bits
+	 * so 1C = 825 / 25 = 33 bits
+	 */
+    uint16_t val = ADS1015_SingleEnded(&hi2c1, ADS1015_ADDRESS, 1, ADS1015_GAIN_TWO);
+
+    /*
+     * val goes down as it gets hotter ( sigh should have the resistor on ground)
+     *
+     * 26C = 1650 - 66 = 1584 bits
+     * 25C = 1650 bits
+     * 24C = 1650 + 66 = 1716 bits
+     */
+	float deg;
+	if (val < 1650) {
+		deg = 25.0 + ( (1650 - val) / 66.0 );
+	} else {
+		deg = 25.0 - ( (val - 1650) / 66.0 );
+	}
+	return round(deg);
+
+//    int8_t cal = 3; // Calibration shows reading off by this much.
+//    float deg = val / 66.0 - cal;
+//
+//
+//    return round(deg);
+//
+//    // convert the value to resistance
+//
+//    double value = 2047.0 / val - 1;
+//	value = THERMISTOR_SERIES_RESISTOR / value;
+//
+//
+//
+//	double steinhart;
+//	steinhart = (double) value / (double) THERMISTOR_NOMINAL;    // (R/Ro)
+//	steinhart = log(steinhart);                  // ln(R/Ro)
+//	steinhart /= THERMISTOR_BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+//	steinhart += 1.0 / (THERMISTER_TEMPERATURE_NOMINAL + 273.15); // + (1/To)
+//	steinhart = 1.0 / steinhart;                 // Invert
+//	steinhart -= 273.15;
+//
+//	return round(steinhart);
 }
